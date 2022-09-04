54d935b78adfff6c9a8b01164866499c95a7d97d
Ali Alhussein <a.alhussein33@gmail.com>
2022-09-04 15:13:29 +0300

    some styling to modal component

--- js/util/spinner-util.js
+++ js/util/spinner-util.js
@@ -36,45 +36,45 @@ export function getModalComponent(spinner, cssCode) {
       attrs: [{ key: 'class', value: 'modal' }],
       content: [{
         tagName: 'div',
-        attrs: [{ key: 'class', value: 'close' }],
-        content: [{ tagName: 'i', attrs: [{ key: 'class', value: 'fa-solid fa-close' }], content: '' }]
+        attrs: [{ key: 'class', value: 'header' }],
+        content: [{
+          tagName: 'h4',
+          content: spinner.name
+        },
+        {
+          tagName: 'div',
+          attrs: [{ key: 'class', value: 'close' }],
+          content: [{ tagName: 'i', attrs: [{ key: 'class', value: 'fa-solid fa-close' }], content: '' }]
+        }]
       }, {
         tagName: 'div',
-        attrs: [{ key: 'class', value: 'content' }],
+        attrs: [{ key: 'class', value: 'main' }],
         content: [{
           tagName: 'div',
-          attrs: [{ key: 'class', value: 'header' }],
-          content: [{ tagName: 'h3', content: spinner.name }]
+          attrs: [{ key: 'class', value: 'col preview' }],
+          content: [{ tagName: 'h5', content: 'Preview' }, {
+            tagName: 'div',
+            attrs: [{ key: 'class', value: `spinner ${spinner.className}` }],
+            content: spinner.innerContent ? spinner.innerContent.map(inner => ({
+              tagName: 'div',
+              attrs: [{ key: 'class', value: inner.className }],
+              content: ''
+            })) : '',
+          }]
         }, {
           tagName: 'div',
-          attrs: [{ key: 'class', value: 'main' }],
-          content: [{
+          attrs: [{ key: 'class', value: 'col codes' }],
+          content: [{ tagName: 'h5', content: 'HTML' }, {
             tagName: 'div',
-            attrs: [{ key: 'class', value: 'col preview' }],
-            content: [{ tagName: 'h5', content: 'Preview' }, {
-              tagName: 'div',
-              attrs: [{ key: 'class', value: `spinner ${spinner.className}` }],
-              content: spinner.innerContent ? spinner.innerContent.map(inner => ({
-                tagName: 'div',
-                attrs: [{ key: 'class', value: inner.className }],
-                content: ''
-              })) : '',
+            attrs: [{ key: 'class', value: 'code' }],
+            content: [{
+              tagName: 'pre',
+              content: `<div class="spinner ${spinner.className}">${spinner.innerContent ? spinner.innerContent.map(inner => `<div class="${inner.className}"></div>`).toString().split(',').join('') : ''}</div>`
             }]
-          }, {
+          }, { tagName: 'h5', content: 'CSS' }, {
             tagName: 'div',
-            attrs: [{ key: 'class', value: 'col codes' }],
-            content: [{ tagName: 'h5', content: 'HTML' }, {
-              tagName: 'div',
-              attrs: [{ key: 'class', value: 'code' }],
-              content: [{
-                tagName: 'pre',
-                content: `<div class="spinner ${spinner.className}">${spinner.innerContent ? spinner.innerContent.map(inner => `<div class="${inner.className}"></div>`).toString().split(',').join('') : ''}</div>`
-              }]
-            }, { tagName: 'h5', content: 'CSS' }, {
-              tagName: 'div',
-              attrs: [{ key: 'class', value: 'code' }],
-              content: [{ tagName: 'pre', content: cssCode }]
-            }]
+            attrs: [{ key: 'class', value: 'code' }],
+            content: [{ tagName: 'pre', content: cssCode }]
           }]
         }]
       }]
