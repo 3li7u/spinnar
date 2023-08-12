80f3b84a08e63dedecb243a8f8cf1b630cc5fd09
Ali Alhussein <a.alhussein33@gmail.com>
2023-08-12 13:37:32 +0300

    add contribution btn

--- js/data.js
+++ js/data.js
@@ -1,114 +1,114 @@
-let spinnersData = [
+const spinnersData = [
   {
-    name: 'Loading spinner',
-    className: 'spinner-loading',
+    name: "Loading spinner",
+    className: "spinner-loading",
   },
   {
-    name: 'Loading spinner with background',
-    className: 'spinner-loading-bg',
+    name: "Loading spinner with background",
+    className: "spinner-loading-bg",
   },
   {
-    name: 'Loading spinner with tail and background',
-    className: 'spinner-loading-tail-bg',
+    name: "Loading spinner with tail and background",
+    className: "spinner-loading-tail-bg",
   },
   {
-    name: 'Flashing Bubble',
-    className: 'spinner-flash-bubble',
+    name: "Flashing Bubble",
+    className: "spinner-flash-bubble",
   },
   {
-    name: 'Stone effect in the pond',
-    className: 'spinner-stone-pond',
+    name: "Stone effect in the pond",
+    className: "spinner-stone-pond",
   },
   {
-    name: 'Flashing circles',
-    className: 'spinner-flash-circles',
+    name: "Flashing circles",
+    className: "spinner-flash-circles",
     innerContent: [
       {
-        className: 'dot',
+        className: "dot",
       },
       {
-        className: 'dot',
+        className: "dot",
       },
       {
-        className: 'dot',
+        className: "dot",
       },
     ],
   },
   {
-    name: 'Pulsating circles',
-    className: 'spinner-pulsate-circles',
+    name: "Pulsating circles",
+    className: "spinner-pulsate-circles",
     innerContent: [
       {
-        className: 'dot',
+        className: "dot",
       },
       {
-        className: 'dot',
+        className: "dot",
       },
       {
-        className: 'dot',
+        className: "dot",
       },
     ],
   },
   {
-    name: 'Swinging flashing circles',
-    className: 'spinner-swing-flash-circles',
+    name: "Swinging flashing circles",
+    className: "spinner-swing-flash-circles",
     innerContent: [
       {
-        className: 'dot',
+        className: "dot",
       },
       {
-        className: 'dot',
+        className: "dot",
       },
       {
-        className: 'dot',
+        className: "dot",
       },
     ],
   },
   {
-    name: 'Flipping box',
-    className: 'spinner-flip-box',
+    name: "Flipping box",
+    className: "spinner-flip-box",
   },
   {
-    name: 'Wavy lines',
-    className: 'spinner-wav-lines',
+    name: "Wavy lines",
+    className: "spinner-wav-lines",
     innerContent: [
       {
-        className: 'line',
+        className: "line",
       },
       {
-        className: 'line',
+        className: "line",
       },
       {
-        className: 'line',
+        className: "line",
       },
-    ]
+    ],
   },
   {
-    name: 'Audio waves',
-    className: 'spinner-audio-waves',
+    name: "Audio waves",
+    className: "spinner-audio-waves",
     innerContent: [
       {
-        className: 'line',
+        className: "line",
       },
       {
-        className: 'line',
+        className: "line",
       },
       {
-        className: 'line',
+        className: "line",
       },
       {
-        className: 'line',
+        className: "line",
       },
       {
-        className: 'line',
+        className: "line",
       },
       {
-        className: 'line',
+        className: "line",
       },
       {
-        className: 'line',
+        className: "line",
       },
-    ]
+    ],
   },
 ];
 
